6d8c7a9fe9ece9572a3f76055f43b4e3530470d8
Peleus <peleus@netsec.ws>
2015-05-24 15:25:57 +1000

    Starting repo

---
+++ main.go
@@ -0,0 +1,28 @@
+package main 
+
+import (
+    //"fmt"
+    //"net"
+    //"flag"
+    //"bufio"
+)
+
+type Settings struct {
+    TargetList  string
+    Url         string
+}
+
+func check(e error) {
+    if e != nil {
+        panic (e)
+    }
+    return
+}
+
+func parseCommandLine() {
+
+}
+
+func main() {
+    
+}
